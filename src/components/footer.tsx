fd2e2e9c9db3f7cd6d9d190635baa01ffb09e84a
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-07-29 23:57:39 -0300

    Create a footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export function Footer() {
+  return (
+    <footer>
+      <h3>Book Store</h3>
+      <div>
+        <Link href="https:/github.com/EliasPereyra">Github</Link>
+      </div>
+    </footer>
+  );
+}
