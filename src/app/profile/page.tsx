4c29ec00f6773e417767b94f79d057d1c5d5b4fc
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-08-04 17:52:31 -0300

    Feat: create a profile page

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,13 @@
+import { css } from "../../../styled-system/css";
+
+export default function Profile() {
+  return (
+    <section>
+      <h3>Profile</h3>
+      <p>
+        A brief description about the user, and if it&apos;s an author, talk
+        about its works
+      </p>
+    </section>
+  );
+}
