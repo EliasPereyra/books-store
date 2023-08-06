15f0b0752d0d8455208b4faad9f0de2625eff721
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-08-05 23:58:05 -0300

    Fix: align all the elements of the profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,9 +2,36 @@ import { css } from "../../../styled-system/css";
 
 export default function Profile() {
   return (
-    <section>
-      <h3>Profile</h3>
-      <p>
+    <section
+      className={css({
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        p: "4",
+      })}
+    >
+      <div
+        className={css({
+          width: "3em",
+          m: "auto",
+        })}
+      >
+        <img src="" alt="Profile logo" />
+      </div>
+      <h3
+        className={css({
+          fontSize: "4xl",
+          textAlign: "center",
+        })}
+      >
+        Username
+      </h3>
+      <p
+        className={css({
+          textAlign: "center",
+          mb: "20",
+        })}
+      >
         A brief description about the user, and if it&apos;s an author, talk
         about its works
       </p>
